36de695c5d90cf6a7aad65da88f3c4a7699b6848
DorcasAddai <70699528+DorcasAddai@users.noreply.github.com>
2021-11-30 21:46:40 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View} from 'react-native';
+import BikesList from './components/BikesList';
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header/>
+      <BikesList/>
+      <StatusBar style="auto"/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
